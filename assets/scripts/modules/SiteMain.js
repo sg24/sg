7ea087d4ff2a9ddf556967bee96ad1cbeeaa085c
phil <philipmayowa03@gmail.com>
2018-06-21 05:02:59 +0100

    finished all for share post

--- assets/scripts/modules/SiteMain.js
+++ assets/scripts/modules/SiteMain.js
@@ -17,9 +17,10 @@ class SiteMain {
         this.contentItemsOption = $(".site-main__content--details__footer--details__options--item");
         this.contentItemsOptionList = $(".site-main__content--details__footer--details__options--toggle");
         this.userCounter = $(".site-main__content--details__header--share__userCounter");
-        this.userCounterViewAll = $(".site-main__content--details__header--share__userCounter__viewall");
+        this.userCounterViewAll = $(".site-main__content--details__header--share__userCounter__viewall, .site-main__content--details__header--share__userCounter-sm--secondItem");
         this.userSelected = $(".site-main__content--details__header--share__userSelected");
         this.userSelectedClosed = $(".site-main__content--details__header--share__userSelected--mainclose");
+        this.userSelectedSmWrapper = $(".site-main__content--details__header--share__userCounter-sm");
         this.events(); 
     }
 
@@ -52,6 +53,7 @@ class SiteMain {
         this.shareSocialIcons.toggleClass("site-main__content--details__header--share__options--socialIcons__socialIconHide");
         this.userCounter.toggleClass("site-main__content--details__header--share__userCounter--visible");
         this.userSelected.removeClass("site-main__content--details__header--share__userSelected--visible");
+        this.userSelectedSmWrapper.toggleClass("site-main__content--details__header--share__userCounter-sm--visible")
     }
 
     addUserSelectIcon() {
@@ -90,6 +92,12 @@ class SiteMain {
         this.userSelected.removeClass("site-main__content--details__header--share__userSelected--visible");
         this.userList.toggleClass("site-main__content--details__header--share__content__visible");
     }
+
+    // showSelectedUsersSm () {
+    //     this.userSelected.toggleClass("site-main__content--details__header--share__userSelected--visible");
+    //     this.userList.removeClass("site-main__content--details__header--share__content__visible");
+    //     this.shareSocialIcons.addClass("site-main__content--details__header--share__options--socialIcons__socialIconHide");
+    // }
 }
 
 export default SiteMain;
