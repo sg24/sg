4c1ce9353654b9a625eaf02f71cfa704cbca3ad5
phil <philipmayowa03@gmail.com>
2018-07-25 00:11:09 +0100

    finished private chat

--- assets/privatechatscript/modules/Chat.js
+++ assets/privatechatscript/modules/Chat.js
@@ -7,6 +7,10 @@ class Chat {
         this.closePvtChat = $(".site-main__pvtchat--main__msg, .site-main__pvtchat--main__chatdate");
         this.chatDetToggler = $(".site-main__pvtchat--main__msg--footer__content");
         this.chatDet = $(".site-main__pvtchat--main__msg--footer__content--details");
+        this.chatOptToggler = $(".site-main__pvtchat--main__options--searchuser__chat--img");
+        this.chatOpt = $(".site-main__pvtchat--main__options--searchuser__chat--img__userdetails");
+        this.srchUserToggler = $(".site-main__pvtchat--main__options--create");
+        this.srchUser = $(".site-main__pvtchat--main__options--searchuser");
         this.events();
     }
 
@@ -14,6 +18,9 @@ class Chat {
         this.pvtChatToggler.click(this.togglePvtChat.bind(this));
         this.closePvtChat.click(this.removePvtChat.bind(this));
         this.chatDetToggler.click(this.toggleChatDet.bind(this));
+        this.chatOptToggler.mouseenter(this.addChatOpt.bind(this));
+        this.chatOptToggler.mouseleave(this.removeChatOpt.bind(this));
+        this.srchUserToggler.click(this.toggleSrchUser.bind(this));
     }
 
     togglePvtChat() {
@@ -26,6 +33,18 @@ class Chat {
 
     toggleChatDet() {
         this.chatDet.toggleClass("site-main__pvtchat--main__msg--footer__content--details__visible");
+    }
+
+    addChatOpt() {
+        this.chatOpt.addClass("site-main__pvtchat--main__options--searchuser__chat--img__userdetails--visible");
+    }
+
+    removeChatOpt() {
+        this.chatOpt.removeClass("site-main__pvtchat--main__options--searchuser__chat--img__userdetails--visible");
+    }
+
+    toggleSrchUser() {
+        this.srchUser.toggleClass("site-main__pvtchat--main__options--searchuser__visible");
     }
 }
 
