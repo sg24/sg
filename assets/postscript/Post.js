f2801bbaca9aea4b1a47a3fd2ca91f2a9c1e47a3
phil <philipmayowa03@gmail.com>
2019-01-18 09:31:51 +0100

    finished add user opt and share opt to post page

--- assets/postscript/Post.js
+++ assets/postscript/Post.js
@@ -1,5 +1,9 @@
 import PostFilter from './modules/PostFilter';
 import PostScroll from './modules/PostScroll'; 
+import Share from './modules/Share';
+import UserOpt from './modules/UserOpt';
 
 var postFilter = new PostFilter();
 var postScroll = new PostScroll();  
+var share = new Share();
+var userOpt = new UserOpt();
--- assets/postscript/modules/PostScroll.js
+++ assets/postscript/modules/PostScroll.js
@@ -4,7 +4,7 @@ import waypoints from "waypoints/lib/noframework.waypoints";
 class PostScroll {
     constructor() {
         this.menu = $(".site-main");
-        this.postTab = $(".site-main__content--details__tab");
+        this.postTab = $(".site-main__content--tab");
         this.siteNav = $(".site-main__nav");
         this.siteHeaderList = $(".site-header__list");
         this.postSticky();
@@ -16,11 +16,11 @@ class PostScroll {
             element: this.menu[0], 
             handler: function(direction) { 
                 if (direction == "down") { 
-                    that.postTab.addClass("site-main__content--details__tab--sticky");
+                    that.postTab.addClass("site-main__content--tab__sticky");
                     that.siteHeaderList.addClass("site-header__list--hidden");
                     that.siteNav.addClass("site-main__nav--sticky");
                 } else { 
-                    that.postTab.removeClass("site-main__content--details__tab--sticky");
+                    that.postTab.removeClass("site-main__content--tab__sticky");
                     that.siteHeaderList.removeClass("site-header__list--hidden");
                     that.siteNav.removeClass("site-main__nav--sticky");
                 }
