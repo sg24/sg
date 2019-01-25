746a078ad3e872026e80c32690afd80e5faa0ef8
phil <philipmayowa03@gmail.com>
2019-01-25 13:26:19 +0100

    finished add share opt to index page

--- assets/scripts/appScript.js
+++ assets/scripts/appScript.js
@@ -1,24 +1,8 @@
-import SiteHeader from './modules/SiteHeader';
-import ToolTips from './modules/ToolTips';
-import Notify from './modules/Notify';
-import SiteMain from './modules/SiteMain';
-import SiteMainGroup from './modules/SiteMainGroup';
-import SiteMainHelpMe from './modules/SiteMainHelpMe';
-import SiteMainCommunities from './modules/SiteMainCommunities';
-import SiteNav from './modules/SiteNav';
-import SiteMainOnlineQue from './modules/SiteMainOnlineQue';
-import SiteMainChat from './modules/SiteMainChat';
-import Scroll from './modules/Scroll';
+import Scroll from './modules/Scroll'; 
+import Share from './modules/Share';
+import UserOpt from './modules/UserOpt';
 
-var user = new SiteHeader();
-var toolTips = new ToolTips();
-var notify = new Notify();
-var mainContent = new SiteMain();
-var siteMainGroup = new SiteMainGroup();
-var siteMainHelpMe = new SiteMainHelpMe();
-var siteMainCommunities = new SiteMainCommunities();
-var siteNav = new SiteNav();
-var siteOnlineQue = new SiteMainOnlineQue();
-var siteMainChat = new SiteMainChat();
-var scroll = new Scroll();
+var scroll = new Scroll(); 
+var share = new Share();
+var userOpt = new UserOpt();
 
