1087222f5c79649e2cb7c543d0b5e6c39f65c5f5
phil <philipmayowa03@gmail.com>
2019-02-01 10:33:27 +0100

    finished fixing private chat style and script

--- assets/convscript/conv.js
+++ assets/convscript/conv.js
@@ -1,5 +1,3 @@
 import Chat from './modules/Chat';
-import ConvScroll from './modules/ConvScroll';
 
 var chat = new Chat();
-var convScroll = new ConvScroll(); 
--- assets/convscript/modules/Chat.js
+++ assets/convscript/modules/Chat.js
@@ -2,51 +2,51 @@ import $ from 'jquery';
 
 class Chat {
     constructor() {
-        this.overlay = $(".site-main__conv--tab__overlay");
-        this.convDetToggler = $(".site-main__conv--tab__content--opt");
-        this.convDetOpt = $(".site-main__conv--tab__content--opt__det");
+        this.overlay = $(".site-main__content--overlay");
+        this.convDetToggler = $(".site-main__conv--content__opt");
+        this.convDetOpt = $(".site-main__conv--content__opt--det");
         this.srchToggler = $(".site-main__conv--header__search");
-        this.srch = $(".site-main__conv--tab__srch");
-        this.srchClose = $(".site-main__conv--tab__srch--close, .site-main__conv--tab__content");
+        this.srch = $(".site-main__conv--srch");
+        this.srchClose = $(".site-main__conv--srch__close, .site-main__conv--content");
         this.sortToggler = $(".site-main__conv--filter__sort");
         this.sortOpt = $(".site-main__conv--filter__sort--opt");
-        this.closeAllToggler = $(".site-main__conv--tab__overlay");
         this.events();
 
     }
 
     events() { 
         this.convDetToggler.click(this.toggleConvDet.bind(this));
-        this.closeAllToggler.click(this.closeAll.bind(this));
+        this.overlay.click(this.closeAll.bind(this));
         this.srchToggler.click(this.toggleSrch.bind(this));
         this.srchClose.click(this.removeSrch.bind(this));  
         this.sortToggler.click(this.toggleSort.bind(this));
     }
 
     toggleConvDet() {
-        this.overlay.toggleClass("site-main__conv--tab__overlay--visible");
-        this.convDetOpt.toggleClass("site-main__conv--tab__content--opt__det--visible");
+        this.overlay.toggleClass("site-main__content--overlay__visible");
+        this.convDetOpt.toggleClass("site-main__conv--content__opt--det__visible");
     }
 
     toggleSrch() {
-        this.srch.toggleClass("site-main__conv--tab__srch--visible");
-        this.overlay.removeClass("site-main__conv--tab__overlay--visible");
-        this.convDetOpt.removeClass("site-main__conv--tab__content--opt__det--visible");
+        this.srch.toggleClass("site-main__conv--srch__visible");
+        this.overlay.toggleClass("site-main__content--overlay__visible");
+        this.convDetOpt.removeClass("site-main__conv--content__opt--det__visible");
         this.sortOpt.removeClass("site-main__conv--filter__sort--opt__visible");
     }   
 
     removeSrch() {
-        this.srch.removeClass("site-main__conv--tab__srch--visible");
+        this.srch.removeClass("site-main__conv--srch__visible");
+        this.overlay.removeClass("site-main__content--overlay__visible");
     }
 
     toggleSort() {
         this.sortOpt.toggleClass("site-main__conv--filter__sort--opt__visible");
-        this.overlay.toggleClass("site-main__conv--tab__overlay--visible");
+        this.overlay.toggleClass("site-main__content--overlay__visible");
     }
 
     closeAll() {
-        this.overlay.removeClass("site-main__conv--tab__overlay--visible");
-        this.convDetOpt.removeClass("site-main__conv--tab__content--opt__det--visible");
+        this.overlay.removeClass("site-main__content--overlay__visible");
+        this.convDetOpt.removeClass("site-main__conv--content__opt--det__visible");
         this.sortOpt.removeClass("site-main__conv--filter__sort--opt__visible");
     }
 }
