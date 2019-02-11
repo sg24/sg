77f5788140dae2303e6defb90b55836039f2bf09
phil <philipmayowa03@gmail.com>
2019-02-11 13:41:11 +0100

    finished adding reuse filter and category to post page

--- assets/postscript/Post.js
+++ assets/postscript/Post.js
@@ -1,7 +1,7 @@
-import PostFilter from './modules/PostFilter';
+import Filter from './modules/Filter';
 import Share from './modules/Share';
 import UserOpt from './modules/UserOpt';
 
-var postFilter = new PostFilter();
+var filter = new Filter();
 var share = new Share();
 var userOpt = new UserOpt();
