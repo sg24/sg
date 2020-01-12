61468db3543c4cddc4f90abc78377c6a65c3f201
phil <philipmayowa03@gmail.com>
2020-01-12 23:44:31 +0100

    finished changed all site pages page url

--- react/signup/src/axios.js
+++ react/signup/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: window.location.origin
+    baseURL: window.location.protocol + '//' + window.location.host
 });
 instance.defaults.headers.common['authorization'] = 'authorization';
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
