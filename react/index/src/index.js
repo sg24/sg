b6063d4d82c281d91dccec6692cdb231bc3e4663
phil <philipmayowa03@gmail.com>
2020-05-02 19:28:23 +0100

    adding polyfill to index page

--- react/index/src/index.js
+++ react/index/src/index.js
@@ -1,9 +1,6 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import 'events-polyfill';
-import 'core-js/features/array/find';
-import 'core-js/features/array/includes';
-import 'core-js/features/number/is-nan';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
