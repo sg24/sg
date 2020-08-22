19e7814100afa55093a46ee60b24d485e3fb7439
phili01 <philipmayowa02@gmail.com>
2020-08-22 12:20:24 +0100

    starting react native

---
+++ rnative/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: '600', fontFamily: 'Roboto-Thinitalic'}}>TExt</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
