7dcd9f50fc911aabfe5bf3f831ff97e9b718c097
phili01 <philipmayowa02@gmail.com>
2020-08-22 12:23:17 +0100

    starting buildng android/ios app

--- rnative/App.js
+++ rnative/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <View>
-        <Text style={{ fontSize: 20, fontWeight: '600', fontFamily: 'Roboto-Thinitalic'}}>TExt</Text>
+        <Text style={{ fontSize: 20, fontWeight: '600', fontFamily: 'Roboto-Thinitalic', fontStyle: 'italic'}}>TExt</Text>
       </View>
     )
   }
