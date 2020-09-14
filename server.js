88c8d93cab544fe999f8d9d1e3478bcb4dd08be8
sg24 <57816740+sg24@users.noreply.github.com>
2020-09-14 08:33:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,53 @@ app.use(function(req, res, next) {
       // }
   next();
 });
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = ' ';
+  let isFinished = false;
+  let isDataSent = false;
 
+  res.once('finish', () => {
+    isFinished = true;
+  });
+
+  res.once('end', () => {
+    isFinished = true;
+  });
+
+  res.once('close', () => {
+    isFinished = true;
+  });
+
+  res.on('data', (data) => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 20000);
+     };
+
+  waitAndSend();
+  next();
+};
+
+app.use(extendTimeoutMiddleware);
 app.set('trust proxy', true);
   
 require('./serverDB/config/passport').auth(passport);
