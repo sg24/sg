0202273cd384657e333841239a0bb99f5cd4ab63
sg24 <57816740+sg24@users.noreply.github.com>
2020-09-14 07:32:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(function(req, res, next) {
     // }
       let host = req.headers.host;
       if (!host.match(/^www\..*/i)) {
-        return res.redirect(301, "http://www." + host + req.url);
+        return res.redirect(301, "https://www." + host + req.url);
       } 
       // else if (req.headers['x-forwarded-proto'] !== 'https') {
       //   return res.redirect('https://' + req.hostname + req.url);
