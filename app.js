f457480f81a24edbc7c6515239b9b9611fd0c4c8
sg24 <philipmayowa01@gmail.com>
2021-06-27 23:06:21 +0100

    testing vercel

---
+++ app.js
@@ -0,0 +1,377 @@
+const express = require('express');
+const path = require('path');
+var favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const useragent = require('express-useragent');
+
+// let appRoutes = require('./routes/app');
+// let usersRoutes = require('./routes/users');
+// let formRoutes = require('./routes/form');
+// let authRoutes = require('./routes/auth');
+// let sitemapxmlRoute = require('./routes/sitemapxml');
+// let postRoute = require('./routes/post');
+// let questionRoute = require('./routes/question');
+// let poetRoute = require('./routes/poet');
+// let favoriteRoute = require('./routes/favorite');
+// let profileRoute = require('./routes/profile');
+// let viewRoute = require('./routes/view');
+// let shareRoute = require('./routes/share');
+// let mediaRoute = require('./routes/media');
+// let groupRoute = require('./routes/group');
+
+let app = express();
+
+// let sess = {
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {
+//     maxAge: 7257600000
+//   }
+// }
+ 
+// if (app.get('env') === 'production') {
+//   app.set('trust proxy', 1) // trust first proxy
+//   sess.cookie.secure = true // serve secure cookies
+// }
+ 
+// app.use(session(sess))
+app.use(useragent.express())
+app.use(cookieParser('secret'));
+app.use(function(req, res, next) {
+  // if(req.headers.host.slice(0, 4) === 'www.') {
+  //     var newHost = req.headers.host.slice(4);
+  //     //   return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  //     return res.redirect(301, 'https://slodge24.com' + req.url);
+  //   }
+    
+    // if(req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+    //   return res.redirect(301, 'https://www.slodge24.com' + req.url);
+    // }
+      // let host = req.headers.host;
+      // if (!host.match(/^www\..*/i)) {
+      //   return res.redirect(301, "https://www." + host + req.url);
+      // } 
+    //  var source = req.headers['user-agent'],
+    //   ua = useragent.parse(source);
+    //   if (ua && ua.isOpera && ua.isMobile && parseInt(ua.source.split('/').pop().split('.')[0]) < 28) {
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     return res.end('<h1 style="width:100%">Please update your browser to atleast operamini version 28  <br> or switch to another browser </h1></body>');
+    //   }
+      // else if (req.headers['x-forwarded-proto'] !== 'https') {
+      //   return res.redirect('https://' + req.hostname + req.url);
+      // }
+  next();
+});
+// const extendTimeoutMiddleware = (req, res, next) => {
+//   const space = ' ';
+//   let isFinished = false;
+//   let isDataSent = false;
+
+//   res.once('finish', () => {
+//     isFinished = true;
+//   });
+
+//   res.once('end', () => {
+//     isFinished = true;
+//   });
+
+//   res.once('close', () => {
+//     isFinished = true;
+//   });
+
+//   res.on('data', (data) => {
+//     // Look for something other than our blank space to indicate that real
+//     // data is now being sent back to the client.
+//     if (data !== space) {
+//       isDataSent = true;
+//     }
+//   });
+
+//   const waitAndSend = () => {
+//     setTimeout(() => {
+//       // If the response hasn't finished and hasn't sent any data back....
+//       if (!isFinished && !isDataSent) {
+//         // Need to write the status code/headers if they haven't been sent yet.
+//         if (!res.headersSent) {
+//           res.writeHead(202);
+//         }
+
+//         res.write(space);
+
+//         // Wait another 15 seconds
+//         waitAndSend();
+//       }
+//     }, 15000);
+//   };
+
+//   waitAndSend();
+//   next();
+// };
+
+// app.use(extendTimeoutMiddleware);
+app.set('trust proxy', true);
+
+app.set('views', path.join(__dirname, 'views'));
+// hbs.registerPartials(__dirname + '/views/partials');
+// hbs.registerPartials(__dirname + '/views/partialsviewque');
+// hbs.registerPartials(__dirname + '/views/partialscomty');
+// hbs.registerPartials(__dirname + '/views/partialspost');
+// hbs.registerPartials(__dirname + '/views/partialsgroup');
+// hbs.registerPartials(__dirname + '/views/partialspoeters');
+// hbs.registerPartials(__dirname + '/views/partialsonlineque');
+// hbs.registerPartials(__dirname + '/views/partialsquestion');
+// hbs.registerPartials(__dirname + '/views/partialsglobal');
+// hbs.registerPartials(__dirname + '/views/partialsgroupchat');
+// hbs.registerPartials(__dirname + '/views/partialsconv');
+// hbs.registerPartials(__dirname + '/views/partialsqueform');
+// hbs.registerPartials(__dirname + '/views/partialspostform');
+// hbs.registerPartials(__dirname + '/views/partialsgroupform');
+// hbs.registerPartials(__dirname + '/views/partialsonlinequeform');
+// hbs.registerPartials(__dirname + '/views/partialspoetersform');
+// hbs.registerPartials(__dirname + '/views/partialsloginform');
+// hbs.registerPartials(__dirname + '/views/partialssignupform');
+// hbs.registerPartials(__dirname + '/views/partialssignprfform');
+// hbs.registerPartials(__dirname + '/views/partialsexam');
+// hbs.registerPartials(__dirname + '/views/partialsusers');
+// hbs.registerPartials(__dirname + '/views/partialsprofile');
+// hbs.registerPartials(__dirname + '/views/partialsfavorite');
+// hbs.registerPartials(__dirname + '/views/partialsviewpt');
+// hbs.registerPartials(__dirname + '/views/partialsviewpwt');
+// hbs.registerPartials(__dirname + '/views/partialsacc');
+// hbs.registerPartials(__dirname + '/views/partialsforgetpwd');
+hbs.registerPartials(__dirname + '/views/term');
+hbs.registerPartials(__dirname + '/views/policy');
+// hbs.registerPartials(__dirname + '/views/editpoet');
+// hbs.registerPartials(__dirname + '/views/editpost');
+// hbs.registerPartials(__dirname + '/views/editque');
+// hbs.registerPartials(__dirname + '/views/share');
+
+app.set('view engine', 'hbs');
+
+// // uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json({limit: '100tb'}));
+app.use(bodyParser.urlencoded({extended: false, limit: '100tb', parameterLimit: 1099511627776}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, data-categ, authorization, limit, skip');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+let appRoutes = require('./routes/app');
+let usersRoutes = require('./routes/users');
+let userchatRoutes = require('./routes/userchat');
+let formRoutes = require('./routes/form');
+let sitemapxmlRoute = require('./routes/sitemapxml');
+let feedRoute = require('./routes/feed');
+let feedchatRoute = require('./routes/feedchat');
+let questionRoute = require('./routes/question');
+let questionchatRoute = require('./routes/questionchat');
+let writeupRoute = require('./routes/writeup');
+let writeupchatRoute = require('./routes/writeupchat');
+let favoriteRoute = require('./routes/favorite');
+let profileRoute = require('./routes/profile');
+let viewRoute = require('./routes/view');
+let shareRoute = require('./routes/share');
+let mediaRoute = require('./routes/media');
+let mediachatRoute = require('./routes/mediachat');
+let groupRoute = require('./routes/group');
+let grouppreviewRoute = require('./routes/grouppreview');
+let groupformRoute = require('./routes/group/form');
+let grouppostRoute = require('./routes/group/post');
+let grouppostchatRoute = require('./routes/group/postchat');
+let groupquestionRoute = require('./routes/group/question');
+let groupquestionchatRoute = require('./routes/group/questionchat');
+let groupfeedRoute = require('./routes/group/feed');
+let groupfeedchatRoute = require('./routes/group/feedchat');
+let groupwriteupRoute = require('./routes/group/writeup');
+let groupwriteupchatRoute = require('./routes/group/writeupchat');
+let groupcbtRoute = require('./routes/group/cbt');
+let groupcbtchatRoute = require('./routes/group/cbtchat');
+let chatRoomRoute = require('./routes/group/chatRoom');
+let roomChatRoute = require('./routes/roomChat');
+let chatRoute = require('./routes/chat');
+let convRoute = require('./routes/conv');
+let advertRoute = require('./routes/advert');
+let advertchatRoute = require('./routes/advertchat');
+let postRoute = require('./routes/post');
+let postchatRoute = require('./routes/postchat');
+let contestRoute = require('./routes/contest');
+let qchatRoute = require('./routes/qchat');
+let cbtchatRoute = require('./routes/cbtchat');
+let examtabRoute = require('./routes/examtab');
+let robotonlyRoute = require('./routes/robot');
+
+app.use('/', [appRoutes, formRoutes, groupformRoute, profileRoute, viewRoute]);
+app.use('/users', usersRoutes);
+app.use('/userchat', userchatRoutes);
+app.use('/feed', feedRoute);
+app.use('/feedchat', feedchatRoute);
+app.use('/question', questionRoute);
+app.use('/questionchat', questionchatRoute);
+app.use('/writeup', writeupRoute);
+app.use('/writeupchat', writeupchatRoute);
+app.use('/favorite', favoriteRoute);
+app.use('/share', shareRoute);
+app.use('/media', mediaRoute);
+app.use('/mediachat', mediachatRoute);
+app.use('/chatroom', chatRoomRoute);
+app.use('/roomchat', roomChatRoute);
+app.use('/group', groupRoute);
+app.use('/groupPreview', grouppreviewRoute);
+app.use('/grouppost', grouppostRoute);
+app.use('/grouppostchat', grouppostchatRoute);
+app.use('/groupquestion', groupquestionRoute);
+app.use('/groupquestionchat', groupquestionchatRoute);
+app.use('/groupfeed', groupfeedRoute);
+app.use('/groupfeedchat', groupfeedchatRoute);
+app.use('/groupwriteup', groupwriteupRoute);
+app.use('/groupwriteupchat', groupwriteupchatRoute);
+app.use('/groupcbt', groupcbtRoute);
+app.use('/groupcbtchat', groupcbtchatRoute);
+app.use('/robotonly', robotonlyRoute);
+app.use('/chat', chatRoute);
+app.use('/conv', convRoute);
+app.use('/post', postRoute);
+app.use('/postchat', postchatRoute);
+app.use('/advert', advertRoute);
+app.use('/advertchat', advertchatRoute);
+app.use('/contest', contestRoute);
+app.use('/cbt', qchatRoute);
+app.use('/cbtchat', cbtchatRoute);
+app.use('/exam', examtabRoute);
+app.use('/sitemap.xml', sitemapxmlRoute);
+app.use(function (req, res, next) {
+    return res.redirect('/index/user');
+});
+
+// app.use('/', [appRoutes, formRoutes,profileRoute, viewRoute]);
+// app.use('/auth', authRoutes)
+// app.use('/users', usersRoutes);
+// app.use('/post', postRoute);
+// app.use('/question', questionRoute);
+// app.use('/poet', poetRoute);
+// app.use('/favorite', favoriteRoute);
+// app.use('/share', shareRoute);
+// app.use('/media', mediaRoute);
+// app.use('/group', groupRoute);
+// app.use('/sitemap.xml', sitemapxmlRoute);
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     return res.redirect('/index/post');
+// });
+
+module.exports = {app};
+
+
+// const express = require('express');
+// const path = require('path');
+// var favicon = require('serve-favicon');
+// const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// const hbs = require('hbs');
+// const passport = require('passport');
+
+// let appRoutes = require('./routes/app');
+// let usersRoutes = require('./routes/users');
+// let formRoutes = require('./routes/form');
+// let authRoutes = require('./routes/auth');
+// let sitemapxmlRoute = require('./routes/sitemapxml');
+// let postRoute = require('./routes/post');
+// let questionRoute = require('./routes/question');
+// let poetRoute = require('./routes/poet');
+// let favoriteRoute = require('./routes/favorite');
+// let profileRoute = require('./routes/profile');
+// let viewRoute = require('./routes/view');
+// let shareRoute = require('./routes/share');
+// let mediaRoute = require('./routes/media');
+// let groupRoute = require('./routes/group');
+
+// let app = express();
+
+// app.use(cookieParser('secret'));
+// app.use(function(req, res, next) {
+//     if(req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+//       return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     next();
+//   });
+  
+// require('./serverDB/config/passport').auth(passport);
+// require('./serverDB/config/passport').authFacebook(passport);
+
+// app.set('views', path.join(__dirname, 'views'));
+// hbs.registerPartials(__dirname + '/views/partials');
+// hbs.registerPartials(__dirname + '/views/partialsviewque');
+// hbs.registerPartials(__dirname + '/views/partialscomty');
+// hbs.registerPartials(__dirname + '/views/partialspost');
+// hbs.registerPartials(__dirname + '/views/partialsgroup');
+// hbs.registerPartials(__dirname + '/views/partialspoeters');
+// hbs.registerPartials(__dirname + '/views/partialsonlineque');
+// hbs.registerPartials(__dirname + '/views/partialsquestion');
+// hbs.registerPartials(__dirname + '/views/partialsglobal');
+// hbs.registerPartials(__dirname + '/views/partialsgroupchat');
+// hbs.registerPartials(__dirname + '/views/partialsconv');
+// hbs.registerPartials(__dirname + '/views/partialsqueform');
+// hbs.registerPartials(__dirname + '/views/partialspostform');
+// hbs.registerPartials(__dirname + '/views/partialsgroupform');
+// hbs.registerPartials(__dirname + '/views/partialsonlinequeform');
+// hbs.registerPartials(__dirname + '/views/partialspoetersform');
+// hbs.registerPartials(__dirname + '/views/partialsloginform');
+// hbs.registerPartials(__dirname + '/views/partialssignupform');
+// hbs.registerPartials(__dirname + '/views/partialssignprfform');
+// hbs.registerPartials(__dirname + '/views/partialsexam');
+// hbs.registerPartials(__dirname + '/views/partialsusers');
+// hbs.registerPartials(__dirname + '/views/partialsprofile');
+// hbs.registerPartials(__dirname + '/views/partialsfavorite');
+// hbs.registerPartials(__dirname + '/views/partialsviewpt');
+// hbs.registerPartials(__dirname + '/views/partialsviewpwt');
+// hbs.registerPartials(__dirname + '/views/partialsacc');
+// hbs.registerPartials(__dirname + '/views/partialsforgetpwd');
+// hbs.registerPartials(__dirname + '/views/term');
+// hbs.registerPartials(__dirname + '/views/policy');
+// hbs.registerPartials(__dirname + '/views/editpoet');
+// hbs.registerPartials(__dirname + '/views/editpost');
+// hbs.registerPartials(__dirname + '/views/editque');
+// hbs.registerPartials(__dirname + '/views/share');
+
+// app.set('view engine', 'hbs');
+
+// // uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(bodyParser.json({limit: '100tb'}));
+// app.use(bodyParser.urlencoded({extended: false, limit: '100tb', parameterLimit: 1099511627776}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, data-categ, authorization, limit, skip');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+//     next();
+// });
+
+// app.use('/', [appRoutes, formRoutes,profileRoute, viewRoute]);
+// app.use('/auth', authRoutes)
+// app.use('/users', usersRoutes);
+// app.use('/post', postRoute);
+// app.use('/question', questionRoute);
+// app.use('/poet', poetRoute);
+// app.use('/favorite', favoriteRoute);
+// app.use('/share', shareRoute);
+// app.use('/media', mediaRoute);
+// app.use('/group', groupRoute);
+// app.use('/sitemap.xml', sitemapxmlRoute);
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     return res.redirect('/index/post');
+// });
+
+// module.exports = {app};
